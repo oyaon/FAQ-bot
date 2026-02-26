e0b4db82921b0725ea9bfa677edfecd16355103a
Oyaon Sultan <oyaon.sultan07@gmail.com>
2026-02-27 03:51:04 +0600

    fix: remove unused ConversationModule import from FaqModule

--- src/faq/faq.module.ts
+++ src/faq/faq.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { FaqService } from './faq.service';
 import { FaqController } from './faq.controller';
 import { EmbeddingModule } from '../embedding/embedding.module';
-import { ConversationModule } from '../conversation/conversation.module';
 import { LlmModule } from '../llm/llm.module';
 
 @Module({
-  imports: [EmbeddingModule, ConversationModule, LlmModule],
+  imports: [EmbeddingModule, LlmModule],
   providers: [FaqService],
   controllers: [FaqController],
   exports: [FaqService],
